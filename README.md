60a54f8a441058a6e66b667f6f1e540c5e88279a
deblur99 <deblur99@kakao.com>
2022-12-17 07:40:52 +0000

    added static res dist

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 할 일
-- PostgreSQL 스키마 구성하기
+- PostgreSQL 스키마 구성하기 (완료)
+- 정적 리소스 (index, board) 배포하기
+  - route
+    - / -> index.html
+    - /board -> board.html
 - Node.js의 pg 라이브러리를 통해 서버와 DB 연동하기
-- route별 DB 쿼리 작업하며 응답값 보내기
-- 필요한 경우 Frontend repo의 정적 리소스 보내기
+  - REST API 구현하면서 쿼리 작업 시 필요하여 같이 진행할 것
+  - route별 DB 쿼리 작업하며 응답값 보내기
 
-- 원하는 그림에 맞춰서 요청, 응답 API 구성 후 swagger로 내보내기
+- 원하는 그림에 맞춰서 요청, 응답 API 구성 후 swagger로 배포하기
+
 - 그리고 끝나는 대로 회의 열기.
--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-const http = require("http");
+const port = 8100;
+const fs = require("fs");
 const express = require("express");
+const app = express();
 
-express.bind();
+// 정적 리소스 배포를 위해 현재 main.js 디렉토리를 기본 경로로 사용
+app.use(express.static(__dirname));
+app.use(express.static("res"));
+
+app.get('/', (req, res) => {
+  if (res.statusCode == 200) {
+    res.sendFile("index.html");
+  }
+});
+
+app.get('/board', (req, res) => {
+  if (res.statusCode == 200) {
+    res.sendFile("board.html");
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Listening on ${port} port`);
+});
